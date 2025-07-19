e2b73828b25872f48815073d14fe76599b14a2a4
Ruben <rubengevorkan40203@gmail.com>
2025-07-19 13:23:07 +0300

    add models

--- tg-bot-ai/internal/models/models.go
+++ tg-bot-ai/internal/models/models.go
@@ -1,2 +1,20 @@
 package models
 //Здесь все структруты (запросы, ответы)
+
+type Message struct {
+	Role    string `json:"role"`
+	Content string `json:"content"`
+}
+
+type Body struct {
+	Model    string    `json:"model"`
+	Messages []Message `json:"messages"`
+}
+
+type Response struct {
+	Choices []struct {
+		Message struct {
+			Content string `json:"content"`
+		} `json:"message"`
+	} `json:"choices"`
+}
