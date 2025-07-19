be79ad369dc3a6ed7a89ec5f908bb89ac663826d
Ruben <rubengevorkan40203@gmail.com>
2025-07-19 14:25:06 +0300

    add main logic

--- tg-bot-ai/cmd/main.go
+++ tg-bot-ai/cmd/main.go
@@ -1,1 +1,49 @@
 package main
+
+import (
+	"log"
+	"os"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"tgbotai/internal/config"
+	hndl "tgbotai/internal/bot"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	
+	err := godotenv.Load()
+	if err != nil{
+		log.Fatal(err)
+	}
+
+	config.BOT_TOKEN = os.Getenv("BOT_TOKEN")
+	config.QWEN_API_TOKEN = os.Getenv("QWEN_API")
+
+	bot, err := tgbotapi.NewBotAPI(config.BOT_TOKEN)
+	
+	if err != nil{
+		log.Fatalf("Ошибка в запуске бота")
+	}
+
+	bot.Debug = true
+	log.Printf("Авторизован как %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60 
+	updates := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message == nil {
+			continue 
+		}
+
+		go func ()  {
+			err := hndl.HandlerQWEN(bot, update.Message)
+
+			if err != nil{
+				bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, "Ошибка API"))
+			}
+		}()
+	}
+}
--- tg-bot-ai/go.mod
+++ tg-bot-ai/go.mod
@@ -2,9 +2,12 @@ module tgbotai
 
 go 1.23.6
 
-require github.com/go-resty/resty/v2 v2.16.5
+require (
+	github.com/go-resty/resty/v2 v2.16.5
+	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
+)
 
 require (
-	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	golang.org/x/net v0.33.0 // indirect
 )
