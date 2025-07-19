4428e8cf517c3abb57563394bd0eba3e8fac3384
Ruben <rubengevorkan40203@gmail.com>
2025-07-19 13:49:23 +0300

    add QWEN API + EditResponse()

--- tg-bot-ai/internal/openrouterai/client.go
+++ tg-bot-ai/internal/openrouterai/client.go
@@ -1,2 +1,40 @@
 package openrouterai
+
 //Здесь логика работа с апи ИИ
+
+import (
+	"encoding/json"
+	"tgbotai/internal/config"
+	"tgbotai/internal/models"
+	"tgbotai/internal/utils"
+	"github.com/go-resty/resty/v2"
+)
+
+func CallQWEN(prompt string) (string, error) {
+	client := resty.New()
+
+	request := &models.Body{
+		Model: "qwen/qwq-32b:free",
+		Messages: []models.Message{
+			{Role: "user", Content: prompt},
+		},
+	}
+
+	resp, err := client.R().SetHeader("Authorization", config.QWEN_API_TOKEN).SetHeader("Content-Type", "application/json").SetBody(request).
+		Post("https://openrouter.ai/api/v1/chat/completions")
+
+	if err != nil {
+		return "", err
+	}
+
+	result := &models.Response{}
+	err = json.Unmarshal(resp.Body(), result)
+
+	if err != nil {
+		return "",err
+	}
+
+	res := utils.EditResult(result.Choices[0].Message.Content)
+	
+	return res, nil
+}
--- tg-bot-ai/internal/utils/format.go
+++ tg-bot-ai/internal/utils/format.go
@@ -1,2 +1,13 @@
 package utils
 //Здесь форматируются строки
+import (
+	"strings"
+)
+
+func EditResult(result string) string {
+	result = strings.ReplaceAll(result, `\n`, "\n")
+	result = strings.ReplaceAll(result, "*", "")
+	result = strings.ReplaceAll(result, "#", "")
+
+	return result
+}
